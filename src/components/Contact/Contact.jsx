2e8508b511f6aff11ef38caf27a4bd60b052976d
Elena Zhuvak <elenkazh@gmail.com>
2024-12-28 16:15:58 +0000

    styles for contatcs list

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,16 +1,19 @@
 import { useDispatch } from 'react-redux'
 import css from './Contact.module.css'
 import { deleteContact } from '../../redux/contactsSlice'
+import { FaUser } from "react-icons/fa";
+import { FaUserTimes } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa6";
 
 const Contact = ({data: {id, name, number}}) => {
   const dispatch = useDispatch()
   return (
     <div className={css.contactItem}>
         <div className={css.contactInfo}>
-          <p className={css.contactName}>{name}</p>
-          <p className={css.contactNumber}>{number}</p>
+          <p className={css.contactName}><FaUser className={css.contactIcons}/> {name}</p>
+          <p className={css.contactNumber}><FaPhone className={css.contactIcons}/> {number}</p>
         </div>
-        <button onClick={() => dispatch(deleteContact(id))}>Delete</button>
+        <button className={css.contactBtnDelete} onClick={() => dispatch(deleteContact(id))}>Delete <FaUserTimes className={css.iconBtn}/></button>
     </div>
   )
 }
