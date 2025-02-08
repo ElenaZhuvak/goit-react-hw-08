0c400ee867d2cfdcc74bd89ebbdd31bf88530c01
Elena Zhuvak <elenkazh@gmail.com>
2025-02-08 18:25:38 +0000

    changes contactPage

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -2,11 +2,9 @@ import { useSelector } from 'react-redux';
 import ContactForm from '../../components/ContactForm/ContactForm';
 import ContactList from '../../components/ContactList/ContactList';
 import SearchBox from '../../components/SearchBox/SearchBox';
-import { selectContacts } from '../../redux/contacts/selectors';
 import { selectIsLoggedIn } from '../../redux/auth/selectors';
 
 const ContactsPage = () => {
-  const contacts = useSelector(selectContacts);
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
   return (
