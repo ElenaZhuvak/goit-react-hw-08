4c8fb1b69b2107ce5e4eac933e9f4298435f338d
Elena Zhuvak <elenkazh@gmail.com>
2025-02-11 21:39:05 +0000

    adds HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,24 @@
+import backgroundImage from '../../images/leather.jpg'
+
 const HomePage = () => {
   return (
-    <div>
-      <h2 className="flex items-center justify-center my-4">Your Phonebook 📔</h2>
+    <div
+      className="hero h-screen overflow-hidden"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat'
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-neutral-content text-center">
+       
+          <div className="bg-[rgba(139,69,19,0.5)] w-full">
+            <h1 className="text-5xl font-bold py-4">Phonebook</h1>
+          </div>
+    
+      </div>
     </div>
   );
 };
